ba4f97a03c07b11796448317e82bb68ff4d9cd56
gustavo.pinto <gustavo.pinto@ypse.com.br>
2019-03-05 19:53:46 -0300

    teste prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url: 'http://delineaapi.herokuapp.com/candidate/',
+  url: 'https://delineaapi.herokuapp.com/candidate/',
   client_id: 'Rb6yDNb6muY6Wr9iGybl193VzO6BqOuleLGblg14',
   client_secret:'NjsLaIedGub9LC2xAKHIt7kiN4DiSBLolT74w2PYrOu4PPdRxCNqgZDLS1UlqwSQry2HSmRj21MWcOiKOuLq8UtsD0LBic26SxJAEHqf7AaZ5C6sOSG9WrHf3gOzJkmY'
 };
